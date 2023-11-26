2a0a675cb1a78d338500efaaa6da02b7fe15038d
aftab yunus mulani <aftab.mulani1515@gmail.com>
2023-11-27 03:54:19 +0530

    added useEffect for al calls

--- components/shared/NoResult.js
+++ components/shared/NoResult.js
@@ -4,14 +4,14 @@ import React from 'react';
 const NoResult = ({ title, description }) => {
   return (
     <div className="flex flex-col items-center justify-center w-full mt-10">
-      <img
+      <Image
         src="/assets/images/light-Illustration.png"
         alt="No result found"
         width={270}
         height={200}
         className="block object-contain dark:hidden"
       />
-      <img
+      <Image
         src="/assets/images/dark-Illustration.png"
         alt="No result found"
         width={270}
