95ae4c3ae341154b9e9e48f1e5f43e97578f4469
aftab yunus mulani <aftab.mulani1515@gmail.com>
2023-11-27 04:29:02 +0530

    fixed homepage image issue

--- components/shared/NoResult.js
+++ components/shared/NoResult.js
@@ -1,18 +1,20 @@
 import Image from 'next/image';
 import React from 'react';
+import lightillustration from '../../public/assets/images/light-illustration.png';
+import darkillustration from '../../public/assets/images/dark-illustration.png';
 
 const NoResult = ({ title, description }) => {
   return (
     <div className="flex flex-col items-center justify-center w-full mt-10">
       <Image
-        src="/assets/images/light-Illustration.png"
+        src={lightillustration}
         alt="No result found"
         width={270}
         height={200}
         className="block object-contain dark:hidden"
       />
       <Image
-        src="/assets/images/dark-Illustration.png"
+        src={darkillustration}
         alt="No result found"
         width={270}
         height={200}
